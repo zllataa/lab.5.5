477cb7f076f3f4f89a8b2d1da274b6944565b989
Zlata Polovka <91230475+zllataa@users.noreply.github.com>
2021-11-23 17:37:20 +0300

    Add files via upload

---
+++ lab.5.5/lab.5.5/lab.5.5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int f(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+
+	else if (n % 2 == 0)
+	{
+		return 2 * f(n/2) - 1;
+	}
+
+	else if (n % 2 == 1)
+	{
+		return 2 * f(n / 2) + 1;
+	}
+}
+
+int main()
+{
+	int n;
+	cout << "n= "; cin >> n;
+	cout << "k= " << f(n/2) << endl;
+	cout << "x= " << f(n) << endl;
+	return 0;
+
+}
